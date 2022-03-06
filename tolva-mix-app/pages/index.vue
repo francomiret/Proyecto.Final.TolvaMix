d2c0f47b5737c434d42ca2918df95fe80ec0ee01
franco <francomiret11@gmail.com>
2022-03-05 22:43:51 -0300

    feat: add home view and pages files

--- tolva-mix-app/pages/index.vue
+++ tolva-mix-app/pages/index.vue
@@ -1,83 +1,51 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-col>
+    <h1>Áreas</h1>
+    <v-row>
+      <v-col v-for="app in appliations" :key="app.id" cols="4">
+        <v-card height="200" :href="app.to">
+          <v-card-title>
+            <v-icon> {{ app.icon }}</v-icon>
+            {{ app.name }}
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-col>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      appliations: [
+        {
+          id: 1,
+          name: "Producción",
+          icon: "mdi-account-hard-hat",
+          to: "/produccion",
+        },
+        {
+          id: 2,
+          name: "Ventas",
+          icon: "mdi-account-cash",
+          to: "/ventas",
+        },
+        {
+          id: 3,
+          name: "Oficina Técnica",
+          icon: "mdi-cog",
+          to: "/oficina-tecnica",
+        },
+        {
+          id: 4,
+          name: "Pañol",
+          icon: "mdi-semantic-web",
+          to: "/deposito",
+        },
+      ],
+    };
+  },
+};
 </script>
---
+++ tolva-mix-app/pages/produccion.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Produccion</h1>
+    <NuxtLink to="/"> Home page </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Produccion",
+};
+</script>
