4c1fe57f882870b9580654277f181898eaa827c4
marezelej <marlej1997@gmail.com>
2022-03-07 18:40:40 +0100

    feat: Add custom nav bar component

---
+++ tolva-mix-app/components/base/MyNavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="'menu-item-' + i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon to="/">
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <slot/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
